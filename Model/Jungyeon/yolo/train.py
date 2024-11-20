c96038cdcb7bc95a11f6520e1f8afec6a5bfd703
Jungyeon <seocindy2002@gmail.com>
2024-11-20 16:53:44 +0900

    Fix : fix file paths

--- Model/Jungyeon/yolo/train.py
+++ Model/Jungyeon/yolo/train.py
@@ -1,10 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo11x-seg.pt")  # load a pretrained model (recommended for training)
+model = YOLO("your pretrained model")  # load a pretrained model (recommended for training)
 
 # Train the model
-results = model.train(data="/data/ephemeral/home/Jungyeon/Yolo/datasets/datasets.yaml", 
-                      epochs=100, 
-                      batch = 4,
-                      imgsz=1024)
+results = model.train(data="your dataset yaml file", epochs=100, batch = 4, imgsz=2048)
