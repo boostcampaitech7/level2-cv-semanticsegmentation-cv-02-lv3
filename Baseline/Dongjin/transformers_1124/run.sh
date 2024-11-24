22c56e8c8b7de5a7660685324dc8a19b2abe8264
Dongjin <jinlee.april@gmail.com>
2024-11-24 10:33:56 +0000

    Chore : backup

---
+++ Baseline/Dongjin/transformers_1124/run.sh
@@ -0,0 +1,3 @@
+python /data/ephemeral/home/Dongjin/level2-cv-semanticsegmentation-cv-02-lv3/Baseline/Dongjin/transformers_1124/main.py --exp_path conf/exp10_0.json
+python /data/ephemeral/home/Dongjin/level2-cv-semanticsegmentation-cv-02-lv3/Baseline/Dongjin/transformers_1124/main.py --exp_path conf/exp10_1.json
+python /data/ephemeral/home/Dongjin/level2-cv-semanticsegmentation-cv-02-lv3/Baseline/Dongjin/transformers_1124/main.py --exp_path conf/exp10_2.json
