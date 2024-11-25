b5898cc56183a80b9e89221cd9ee3815d67a18fc
Dongjin <jinlee.april@gmail.com>
2024-11-25 08:50:39 +0000

    Chore : backup

---
+++ Baseline/Dongjin/transformers_1124/run1.sh
@@ -0,0 +1,3 @@
+python inference_ensemble_temp.py --ensemble_path conf/ensemble/ensemble4.json
+python inference_ensemble_temp.py --ensemble_path conf/ensemble/ensemble5.json
+python inference_ensemble_temp.py --ensemble_path conf/ensemble/ensemble6.json
