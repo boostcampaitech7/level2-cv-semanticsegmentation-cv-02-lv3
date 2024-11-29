5a4e7af9a80f25b98ef4936470c2569ecfe75ae7
YU JEONG SEON <yjs000616@gmail.com>
2024-11-29 02:39:07 +0000

    Feat : unet3+ base code

---
+++ models/unet3/train.py
@@ -0,0 +1,130 @@
+# python native
+import os
+import json
+import random
+import datetime
+from functools import partial
+from dotenv import load_dotenv
+# external library
+import cv2
+import numpy as np
+import pandas as pd
+from tqdm.auto import tqdm
+from sklearn.model_selection import GroupKFold
+import albumentations as A
+
+# torch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torchvision import models
+
+# visualization
+import matplotlib.pyplot as plt
+
+import argparse
+
+from dataset import XRayDataset
+
+from trainer import *
+import wandb
+from model import *
+
+import pdb
+
+from loss import *
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train segmentation model")
+    parser.add_argument('--epochs', type=int, default=100, help='Number of epochs')
+    parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate') 
+    parser.add_argument('--batch_size', type=int, default=2, help='Batch size') 
+    parser.add_argument('--valid_batch_size', type=int, default=1, help='valid Batch size') 
+    parser.add_argument('--val_every', type=int, default=1, help='val_every')
+    parser.add_argument('--image_root', type=str,default='/data/ephemeral/home/data/train/DCM',help='image root')
+    parser.add_argument('--label_root',type=str,default='/data/ephemeral/home/data/train/outputs_json',help='label root')
+    parser.add_argument('--saved_dir', type=str, default='checkpoints',help='model checkpoint save')
+    parser.add_argument('--model_name', type=str, default='unet+++', help='Name of the segmentation model')
+    
+    
+    args = parser.parse_args()
+
+    load_dotenv()
+    wandb_api_key = os.getenv('WANDB_API_KEY')
+    wandb.login(key=wandb_api_key)
+
+    # wandb 초기화
+    #wandb.init(entity="luckyvicky",project="segmentation", name=args.model_name,config={
+    #    "epochs": args.epochs,
+    #    "learning_rate": args.lr,
+    #    "batch_size": args.batch_size,
+    #    "valid_batch_size": args.valid_batch_size,
+    #    "model_name": args.model_name
+    #})
+
+
+if not os.path.exists(args.saved_dir):                                                           
+    os.makedirs(args.saved_dir)
+
+RANDOM_SEED = 42  #21
+
+CLASSES = [
+    'finger-1', 'finger-2', 'finger-3', 'finger-4', 'finger-5',
+    'finger-6', 'finger-7', 'finger-8', 'finger-9', 'finger-10',
+    'finger-11', 'finger-12', 'finger-13', 'finger-14', 'finger-15',
+    'finger-16', 'finger-17', 'finger-18', 'finger-19', 'Trapezium',
+    'Trapezoid', 'Capitate', 'Hamate', 'Scaphoid', 'Lunate',
+    'Triquetrum', 'Pisiform', 'Radius', 'Ulna',
+]
+
+def set_seed():
+    torch.manual_seed(RANDOM_SEED)
+    torch.cuda.manual_seed(RANDOM_SEED)
+    torch.cuda.manual_seed_all(RANDOM_SEED) # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(RANDOM_SEED)
+    random.seed(RANDOM_SEED)
+
+# dataset
+tf = A.Resize(512, 512)
+train_dataset = XRayDataset(image_root=args.image_root, label_root=args.label_root, is_train=True, transforms=tf)
+valid_dataset = XRayDataset(image_root=args.image_root, label_root=args.label_root, is_train=False, transforms=tf)
+
+# dataloader
+train_loader = DataLoader(
+    dataset=train_dataset, 
+    batch_size=args.batch_size,
+    shuffle=True,
+    num_workers=4,
+    drop_last=True,
+)
+valid_loader = DataLoader(
+    dataset=valid_dataset, 
+    batch_size=args.valid_batch_size,
+    shuffle=False,
+    num_workers=2,  #0
+    drop_last=False
+)
+
+
+# model
+# 입력 채널: 3 (RGB 이미지), 출력 클래스 수: 29 (다중 클래스 분할)
+model = UNet_3Plus(in_channels=3, n_classes=29, feature_scale=4, is_deconv=True, is_batchnorm=True)
+
+# Loss function
+criterion = nn.BCEWithLogitsLoss()
+
+# Optimizer
+#optimizer = optim.Adam(params=model.parameters(), lr=args.lr, weight_decay=1e-6)
+optimizer = optim.RMSprop(params=model.parameters(), lr=args.lr, weight_decay=1e-6)
+
+# 시드를 설정합니다.
+set_seed()
+
+train(model, train_loader, valid_loader, criterion, optimizer, args.epochs, args.val_every, args.saved_dir, args.model_name)
+
+#wandb.finish()
---
+++ models/unet3/trainer.py
@@ -0,0 +1,187 @@
+# python native
+import os
+import json
+import random
+import datetime
+from functools import partial
+# external library
+import cv2
+import numpy as np
+import pandas as pd
+from tqdm.auto import tqdm
+from sklearn.model_selection import GroupKFold
+import albumentations as A
+# torch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torchvision import models
+import wandb
+import copy
+
+from loss import *
+
+CLASSES = [
+    'finger-1', 'finger-2', 'finger-3', 'finger-4', 'finger-5',
+    'finger-6', 'finger-7', 'finger-8', 'finger-9', 'finger-10',
+    'finger-11', 'finger-12', 'finger-13', 'finger-14', 'finger-15',
+    'finger-16', 'finger-17', 'finger-18', 'finger-19', 'Trapezium',
+    'Trapezoid', 'Capitate', 'Hamate', 'Scaphoid', 'Lunate',
+    'Triquetrum', 'Pisiform', 'Radius', 'Ulna',
+]
+
+
+CLASS2IND = {v: i for i, v in enumerate(CLASSES)}
+IND2CLASS = {v: k for k, v in CLASS2IND.items()}
+
+
+def dice_coef(y_true, y_pred):
+    y_true_f = y_true.flatten(2)
+    y_pred_f = y_pred.flatten(2)
+    intersection = torch.sum(y_true_f * y_pred_f, -1)
+    eps = 0.0001
+    return (2. * intersection + eps) / (torch.sum(y_true_f, -1) + torch.sum(y_pred_f, -1) + eps)
+
+
+def save_model(model, saved_dir, file_name):
+    output_path = os.path.join(saved_dir, file_name)
+    torch.save(model, output_path)
+
+
+def validation(epoch, model, data_loader, criterion, thr=0.5):
+    print(f'Start validation #{epoch:2d}')
+    model.eval()
+    dices = []
+    with torch.no_grad():
+        n_class = len(CLASSES)
+        total_loss = 0
+        cnt = 0
+        for step, (images, masks) in tqdm(enumerate(data_loader), total=len(data_loader)):
+            images, masks = images.cuda(), masks.cuda()
+            model = model.cuda()
+            outputs = model(images)
+            output_h, output_w = outputs.size(-2), outputs.size(-1)
+            mask_h, mask_w = masks.size(-2), masks.size(-1)
+            # gt와 prediction의 크기가 다른 경우 prediction을 gt에 맞춰 interpolation 합니다.
+            if output_h != mask_h or output_w != mask_w:
+                outputs = F.interpolate(outputs, size=(mask_h, mask_w), mode="bilinear")
+            loss = criterion(outputs, masks)
+            total_loss += loss
+            cnt += 1
+            outputs = torch.sigmoid(outputs)
+            outputs = (outputs > thr).detach().cpu()
+            masks = masks.detach().cpu()
+            dice = dice_coef(outputs, masks)
+            dices.append(dice)
+    dices = torch.cat(dices, 0)
+    dices_per_class = torch.mean(dices, 0)
+    dice_str = [
+        f"{c:<12}: {d.item():.4f}"
+        for c, d in zip(CLASSES, dices_per_class)
+    ]
+    dice_str = "\n".join(dice_str)
+    print(dice_str)
+    avg_dice = torch.mean(dices_per_class).item()
+    # 클래스별 dice score도 wandb에 기록
+    dice_dict = {
+        f"{c} Dice": dices_per_class[i].item()
+        for i, c in enumerate(CLASSES)
+    }
+    return avg_dice, dice_dict
+
+
+def train(model, data_loader, val_loader, criterion, optimizer, num_epochs, val_every, saved_dir, model_name):
+    print(f'Start training..')
+    n_class = len(CLASSES)
+    best_dice = 0.
+    for epoch in range(num_epochs):
+        model.train()
+
+        total_loss = 0
+        for step, (images, masks) in enumerate(data_loader):
+            # gpu 연산을 위해 device 할당합니다.
+            images, masks = images.cuda(), masks.cuda()
+         
+            model = model.cuda()
+            outputs = model(images)
+            temp = outputs.detach().clone()  # 계산 그래프에서 분리된 텐서를 복사
+            temp = torch.sigmoid(temp)  # sigmoid 함수 적용
+            #print(temp.sum())
+            
+            # loss를 계산합니다.
+            loss = criterion(outputs, masks)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss
+
+            # step 주기에 따라 loss를 출력합니다.
+            if (step + 1) % 25 == 0:
+                print(
+                    f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} | '
+                    f'Epoch [{epoch+1}/{num_epochs}], '
+                    f'Step [{step+1}/{len(data_loader)}], '
+                    f'Loss: {round(loss.item(),4)}'
+                )
+
+        total_loss /= len(data_loader)
+
+        # # wandb에 현재 loss 로깅
+        # wandb.log({"Train Loss": round(loss.item(), 4)})
+        # validation 주기에 따라 loss를 출력하고 best model을 저장합니다.
+        if (epoch + 1) % val_every == 0:
+            avg_dice, dice_dict= validation(epoch + 1, model, val_loader, criterion)
+            # wandb에 검증 결과 로깅
+            #wandb.log({"Validation Dice": dice, "epoch": epoch + 1})
+            if best_dice < avg_dice:
+                print(f"Best performance at epoch: {epoch + 1}, {best_dice:.4f} -> {avg_dice:.4f}")
+                print(f"Save model in {saved_dir}")
+                best_dice = avg_dice
+                save_model(model, saved_dir, f"{model_name}_best_model.pt")
+            save_model(model, saved_dir, f"epoch_{epoch+1}.pt")
+        log = {
+            "Train Loss": round(total_loss.item(), 4),
+            "Validation Dice": avg_dice
+        }
+        log.update(dice_dict)
+        wandb.log(log)
+
+        
+# mask map으로 나오는 인퍼런스 결과를 RLE로 인코딩 합니다.
+def encode_mask_to_rle(mask):
+    '''
+    mask: numpy array binary mask
+    1 - mask
+    0 - background
+    Returns encoded run length
+    '''
+    pixels = mask.flatten()
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return ' '.join(str(x) for x in runs)
+
+
+
+def test(model, data_loader, thr=0.5):
+    model = model.cuda()
+    model.eval()
+    rles = []
+    filename_and_class = []
+    with torch.no_grad():
+        n_class = len(CLASSES)
+        for step, (images, image_names) in tqdm(enumerate(data_loader), total=len(data_loader)):
+            images = images.cuda()
+            outputs = model(images)
+            outputs = F.interpolate(outputs, size=(2048, 2048), mode="bilinear")
+            outputs = torch.sigmoid(outputs)
+            outputs = (outputs > thr).detach().cpu().numpy()
+            for output, image_name in zip(outputs, image_names):
+                for c, segm in enumerate(output):
+                    rle = encode_mask_to_rle(segm)
+                    rles.append(rle)
+                    filename_and_class.append(f"{IND2CLASS[c]}_{image_name}")
+    return rles, filename_and_class
