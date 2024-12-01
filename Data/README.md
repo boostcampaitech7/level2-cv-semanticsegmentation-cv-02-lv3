62a7e197e9eb40a0c959758946a2f6191c48bc3e
Jungyeon Seo <seocindy2002@gmail.com>
2024-12-02 07:48:59 +0900

    Refactor : refactor code

--- Data/README.md
+++
@@ -1,2 +0,0 @@
-### ⭐ 데이터 관련 실험 ⭐ _ex. Data Aungmentation, Data Cleaning_
-📁 작업 폴더 : '이름' 폴더 만들어서 안에서 작업 진행하기
--- Model/Jungyeon/yolo/train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("your pretrained model")  # load a pretrained model (recommended for training)
-
-# Train the model
-results = model.train(data="your dataset yaml file", epochs=100, batch = 4, imgsz=2048)
